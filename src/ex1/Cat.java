007352543da633c8cd878982de976bde79c79412
ScriptAv <avderuno@gmail.com>
2016-03-04 19:15:20 +0300

    sd

---
+++ src/ex1/Cat.java
@@ -0,0 +1,14 @@
+package ex1;
+
+/**
+ * Created by script on 04.03.16.
+ */
+public class Cat {
+    String name;
+    int age;
+    boolean isMale;
+    Tail tail
+    void voice(){
+        System.out.println("myau");
+    }
+}
---
+++ src/ex1/Tail.java
@@ -0,0 +1,9 @@
+package ex1;
+
+/**
+ * Created by script on 04.03.16.
+ */
+public class Tail {
+    int length;
+    boolean isOk;
+}
