cb602bf70d583d530149e5b121e855871b6aaabf
ScriptAv <avderuno@gmail.com>
2016-03-04 21:03:51 +0300

    sdwed

--- src/ex1/Main2.java
+++ src/ex1/Main2.java
@@ -8,6 +8,14 @@ public class Main2 {
         new Cat("swrgerg", 17); //если со стека нет ссылки на обьект в куче,
                                 //а в куче обьект указывает на другой обьект в куче
                                 //все равно обьект будет уничтожен GC
+        Cat cat1 = new Cat("dfgbhrftb", 65);
+        Cat cat2 = new Cat("dfgbdefbvhrftb", 34);
+
+        cat1.about();
+        //Cat tempCat = cat1;// временная кошка))
+        cat1 = cat2; // теперь cat1 попадет под GC
+        cat1.about();
+        //tempCat.about();
     }
 }
 
